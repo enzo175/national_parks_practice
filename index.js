b5daebbde2ff6b9af4f9cd573d2917169d93bada
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-09-10 19:45:06 +0300

    validation

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const apiKey = "pQ6wyhd76wnAlu3Zt2FcrVAl6a3ahQjSTFGJ8baB";
 
 const searchUrl = 'https://developer.nps.gov/api/v1/parks';
 
-function formatQueryParams(params) { 
+function formatQueryParams(params) {
     const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
 }
 
-function displayResults(responseJson, limit){
+function displayResults(responseJson, limit) {
     console.log(responseJson)
     $('.api-results').empty();
-    for (let i = 0; i < responseJson.data.length; i++){
-    
-    $('.api-results').append(
-    `<ul> 
+    for (let i = 0; i < responseJson.data.length; i++) {
+
+        $('.api-results').append(
+            `<ul> 
     <li><h3>${responseJson.data[i].fullName}</h3>
         <p>${responseJson.data[i].description}</p>
         <p>
@@ -27,42 +27,45 @@ function displayResults(responseJson, limit){
         </p>
     </li>
      `
-    )};
+        )
+    };
 };
 
-function getParks(query, limit=10){
+function getParks(query, limit = 10) {
     const params = {
         q: query,
-        api_key:apiKey,
+        api_key: apiKey,
         language: 'en',
         limit
     };
 
-  const queryString = formatQueryParams(params)
-  const url = searchUrl + '?' + queryString;
+    const queryString = formatQueryParams(params)
+    const url = searchUrl + '?' + queryString;
 
-  console.log(url)
+    console.log(url)
 
-  const options = {
-    // mode: 'cors',
-    // headers: new Headers({
-    //     'Access-Control-Allow-Origin':'*',
-    //     "Access-Control-Allow-Headers":"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+    const options = {
+        // mode: 'cors',
+        // headers: new Headers({
+        //     'Access-Control-Allow-Origin':'*',
+        //     "Access-Control-Allow-Headers":"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
 
-    //     "X-Api-Key": apiKey})
+        //     "X-Api-Key": apiKey})
     };
 
-  fetch(url, options)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-        $('.js-error').text(`Something went wrong: ${err.message}`);
-    });
+    $('.api-results').html(`<p>Please wait...</p>`);
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('.api-results').empty()
+            $('.js-error').text(`Something went wrong: ${err.message}`);
+        })
 }
 
 function watchForm() {
@@ -70,6 +73,10 @@ function watchForm() {
         event.preventDefault();
         const searchTerm = $('#js-parkName').val();
         const limit = $('#js-max-results').val();
+        const numericLimit = parseInt(limit, 10)
+        if (numericLimit <= 0 || numericLimit > 50) {
+            return alert('Limit should be a number between 1 and 50')
+        }
         getParks(searchTerm, limit);
     });
 }
